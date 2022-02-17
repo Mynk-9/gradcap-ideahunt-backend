a47cad1c5f7e8e9b54fab1e4776bbb2786f7de05
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-17 17:07:55 +0530

    Updated user routes and controllers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,27 +1,156 @@
-const mongoose = require('mongoose');
+const { trimUserObject } = require('../utilities/user.utilities');
 const User = require('./../models/user.model');
 
-// TEMPORARY ROUTES FOR TESTING
-const createUser = (req, res) => {
-   const { email, name } = req.body;
-   let newUser = new User({ email: email, name: name });
-   newUser
-      .save()
-      .then(({ _id }) => {
-         res.status(201).send({
-            id: _id,
-         });
-      })
-      .catch(error => {
-         res.status(500).send({
-            Error: error,
-         });
-      });
-};
-const getAll = (req, res) => {
-   User.find()
-      .exec()
-      .then(data => res.status(200).send(data));
+////////////////
+/// HELPERS ////
+////////////////
+
+const getUserField = async (userId, field) => {
+   const resp = await User.findOne({ userId: userId })
+      .lean()
+      .select(field)
+      .exec();
+   return resp[field];
 };
 
-module.exports = { createUser, getAll };
+////////////////
+///// GET //////
+////////////////
+
+const getUser = (req, res, next) => {
+   const userId = req.params.userId;
+   User.findOne({ userId: userId })
+      .select('-ideas')
+      .lean()
+      .exec()
+      .then(user => {
+         user = trimUserObject(user);
+         res.status(200).send(user);
+      })
+      .catch(err => {
+         console.log(err);
+         const error = new Error('Error in processing request');
+         next(error);
+      });
+};
+
+const get = { getUser };
+
+////////////////
+///// POST /////
+////////////////
+
+const postLinkedin = (req, res) => {
+   const _id = req.userData._id;
+   const link = req.body.data;
+
+   User.findOneAndUpdate(
+      { _id },
+      { linkedin: link },
+      { upsert: true, new: true }
+   )
+      .lean()
+      .select('-ideas')
+      .exec()
+      .then(user => {
+         res.status(201).send({ link: user.linkedin });
+      })
+      .catch(error => {
+         res.sendStatus(500);
+         console.log(error);
+      });
+};
+
+const postInstagram = (req, res) => {
+   const _id = req.userData._id;
+   const link = req.body.data;
+
+   User.findOneAndUpdate(
+      { _id },
+      { instagram: link },
+      { upsert: true, new: true }
+   )
+      .lean()
+      .select('-ideas')
+      .exec()
+      .then(user => {
+         res.status(201).send({ link: user.instagram });
+      })
+      .catch(error => {
+         res.sendStatus(500);
+         console.log(error);
+      });
+};
+
+const postTwitter = (req, res) => {
+   const _id = req.userData._id;
+   const link = req.body.data;
+
+   User.findOneAndUpdate(
+      { _id },
+      { twitter: link },
+      { upsert: true, new: true }
+   )
+      .lean()
+      .select('-ideas')
+      .exec()
+      .then(user => {
+         res.status(201).send({ link: user.twitter });
+      })
+      .catch(error => {
+         res.sendStatus(500);
+         console.log(error);
+      });
+};
+
+const postCollege = (req, res) => {
+   const _id = req.userData._id;
+   const college = req.body.data;
+
+   User.findOneAndUpdate(
+      { _id },
+      { college: college },
+      { upsert: true, new: true }
+   )
+      .lean()
+      .select('-ideas')
+      .exec()
+      .then(user => {
+         res.status(201).send({ college: user.college });
+      })
+      .catch(error => {
+         res.sendStatus(500);
+         console.log(error);
+      });
+};
+
+const postGradYear = (req, res) => {
+   const _id = req.userData._id;
+   const date = req.body.data;
+
+   User.findOneAndUpdate(
+      { _id },
+      { gradYear: date },
+      { upsert: true, new: true }
+   )
+      .lean()
+      .select('-ideas')
+      .exec()
+      .then(user => {
+         res.status(201).send({ date: user.gradYear });
+      })
+      .catch(error => {
+         res.sendStatus(500);
+         console.log(error);
+      });
+};
+
+const post = {
+   postLinkedin,
+   postInstagram,
+   postTwitter,
+   postCollege,
+   postGradYear,
+};
+
+module.exports = { ...get, ...post };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
+const { tokenCheck } = require('../auth/googleOAuth.middleware');
 const controllers = require('./../controllers/user.controller');
 
-// TEMPORARY ROUTES
-router.post('/', controllers.createUser);
-router.get('/', controllers.getAll);
+// GET /user
+router.get('/:userId', controllers.getUser);
+
+// POST /user
+router.post('/profile/linkedin', tokenCheck, controllers.postLinkedin);
+router.post('/profile/instagram', tokenCheck, controllers.postInstagram);
+router.post('/profile/twitter', tokenCheck, controllers.postTwitter);
+router.post('/profile/college', tokenCheck, controllers.postCollege);
+router.post('/profile/gradYear', tokenCheck, controllers.postGradYear);
 
 module.exports = router;
