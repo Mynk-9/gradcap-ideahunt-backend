b87d8e5306d8e99b9d3803bf6c6966ad2e34f99c
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-17 15:54:36 +0530

    Added util to generate usernames for new users

--- utilities/user.utilities.js
+++ utilities/user.utilities.js
@@ -1,3 +1,24 @@
+const User = require('../models/user.model');
+
+const userIdGen = async name => {
+   let userNum = 0;
+   let userId = null;
+
+   name = name.split(' ').join('').toLowerCase();
+
+   let otherUser = await User.find({ userId: { $regex: name + '.*' } })
+      .sort({ _id: -1 })
+      .limit(1)
+      .exec();
+   const lastId = otherUser[0]?.userId;
+
+   if (otherUser.length === 0) userNum = 0;
+   else userNum = 1 + parseInt(lastId.substring(name.length, lastId.length));
+
+   userId = name + userNum.toString();
+   return userId;
+};
+
 const trimUserObject = user => {
    let joiningDate = new Date(Date.parse(user.createdAt));
    joiningDate = {
@@ -13,3 +34,5 @@ const trimUserObject = user => {
    delete user.__v;
    return user;
 };
+
+module.exports = { userIdGen, trimUserObject };
