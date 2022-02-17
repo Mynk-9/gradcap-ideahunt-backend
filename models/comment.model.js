16225f1d413df6a031fb4a83899632cb41e99b43
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-17 15:56:05 +0530

    Updated comments schema

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({}, { timestamps: true });
-
-commentSchema.add({
-   data: String,
-   userName: String,
-   userId: { type: mongoose.Types.ObjectId, ref: 'Users' },
-   likes: { type: mongoose.Types.ObjectId, ref: 'Users' },
-   childComments: [commentSchema],
-});
+const commentSchema = new mongoose.Schema(
+   {
+      data: String,
+      user: { type: mongoose.Types.ObjectId, ref: 'Users', required: true },
+      likes: { type: mongoose.Types.ObjectId, ref: 'Users' },
+      parentComment: { type: mongoose.Types.ObjectId, ref: 'Comments' },
+      childComments: [{ type: mongoose.Types.ObjectId, ref: 'Comments' }],
+   },
+   { timestamps: true }
+);
 
 module.exports = mongoose.model('Comments', commentSchema);
