f3ab2b17cd2593370b21654dc0ab83d6ec833961
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-02-17 17:08:12 +0530

    Updated idea routes and controllers

--- controllers/idea.controllers.js
+++ controllers/idea.controllers.js
@@ -1,5 +1,34 @@
 const mongoose = require('mongoose');
 const Idea = require('../models/idea.model');
+const User = require('../models/user.model');
+
+////////////////
+/// HELPERS ////
+////////////////
+
+const likedStatus = async (ideaId, userId) => {
+   let idea = await Idea.findById(ideaId).lean().exec();
+   if (idea.likes.find(val => val.equals(userId))) return true;
+   return false;
+};
+
+const ideasMapper = ideas => {
+   return ideas.map(
+      ({ heading, details, likes, comments, user, _id, featured }) => ({
+         ideaId: String(_id),
+         featured: featured ? true : false,
+         heading: heading,
+         details: details,
+         likes: likes.length,
+         comments: comments.length,
+         profile: {
+            name: user.name,
+            photo: user.profileURL,
+            userId: user.userId,
+         },
+      })
+   );
+};
 
 ////////////////
 ///// GET //////
@@ -10,25 +39,20 @@ const Idea = require('../models/idea.model');
  * All ideas paginated according to given page number and items per page.
  * Default 10 items per page. Default page is 1.
  */
-const getAll = (req, res) => {
-   const pageNumber = req.query.page || 1;
-   const nPerPage = req.query.perPage || 10;
+const getPaginated = (req, res) => {
+   const pageNumber = parseInt(req.query.page) || 1;
+   const nPerPage = parseInt(req.query.perPage) || 10;
 
    Idea.find({})
       .skip(pageNumber > 0 ? (pageNumber - 1) * nPerPage : 0)
+      .limit(nPerPage)
+      .lean()
+      .populate('user')
       .exec()
       .then(ideas => {
          res.status(200).send({
-            ideas: ideas.map(
-               ({ heading, brief, details, likes, comments, user }) => ({
-                  heading: heading,
-                  brief: brief,
-                  details: details,
-                  likes: likes.length,
-                  comments: comments.length,
-                  user: user,
-               })
-            ),
+            count: ideas.length,
+            ideas: ideasMapper(ideas),
          });
       })
       .catch(error => {
@@ -38,7 +62,83 @@ const getAll = (req, res) => {
       });
 };
 
-const gets = { getAll };
+const getCount = (req, res, next) => {
+   Idea.countDocuments({})
+      .lean()
+      .exec()
+      .then(count => {
+         console.log('ideas count', count);
+         res.status(200).send({ count });
+      })
+      .catch(error => {
+         console.log(error);
+         next(new Error('Error in document counting'));
+      });
+};
+
+const isLiked = (req, res) => {
+   const ideaId = mongoose.Types.ObjectId(req.params.ideaId);
+   const userId = mongoose.Types.ObjectId(req.userData._id);
+
+   likedStatus(ideaId, userId)
+      .then(liked => {
+         res.status(200).send({ liked: liked });
+      })
+      .catch(error => {
+         console.log(error);
+         res.sendStatus(500);
+      });
+};
+
+const userIdeas = async (req, res) => {
+   const userId = req.params.userId;
+   let _id = await User.findOne({ userId: userId }).lean().exec();
+
+   Idea.find({ user: _id })
+      .lean()
+      .populate('user')
+      .exec()
+      .then(ideas => {
+         ideas = ideasMapper(ideas);
+         const count = ideas.length;
+         res.status(200).send({ count, ideas });
+      })
+      .catch(error => {
+         console.log(error);
+         res.sendStatus(500);
+      });
+};
+
+const getIdeaDetails = (req, res) => {
+   const ideaId = req.params.ideaId;
+   Idea.findOne({ _id: ideaId })
+      .lean()
+      .populate('user')
+      .select('-comments')
+      .exec()
+      .then(({ heading, details, likes, comments, user, _id, featured }) => {
+         const ideaDetails = {
+            ideaId: String(_id),
+            featured: featured ? true : false,
+            heading: heading,
+            details: details,
+            likes: likes?.length ? likes.length : 0,
+            comments: comments?.length ? comments.length : 0,
+            profile: {
+               name: user.name,
+               photo: user.profileURL,
+               userId: user.userId,
+            },
+         };
+         res.status(200).send(ideaDetails);
+      })
+      .catch(err => {
+         res.sendStatus(500);
+         console.log(err);
+      });
+};
+
+const gets = { getPaginated, getCount, isLiked, userIdeas, getIdeaDetails };
 
 ////////////////
 ///// post /////
@@ -48,29 +148,73 @@ const gets = { getAll };
  * POST /ideas
  * Add new idea. Needed userId, heading, brief, details.
  */
-const postOne = (req, res) => {
-   const { heading, brief, details, userId } = req.body;
+const postIdea = async (req, res) => {
+   const { heading, brief, details } = req.body;
+   const user_id = req.userData._id;
 
-   let newIdea = new Idea({
+   const newIdea = new Idea({
       heading: heading,
       brief: brief,
       details: details,
-      user: mongoose.Types.ObjectId(userId),
+      user: mongoose.Types.ObjectId(user_id),
    });
    newIdea
       .save()
       .then(({ _id }) => {
-         res.status(201).send({
-            id: _id,
-         });
+         User.findByIdAndUpdate(
+            user_id,
+            { $push: { ideas: _id } },
+            { upsert: true, new: true }
+         )
+            .lean()
+            .exec()
+            .then(() => {
+               res.status(201).send({ _id });
+            })
+            .catch(err => {
+               res.status(500).send({ error: 'Error at updating user' });
+               console.log(err);
+            });
       })
-      .catch(error => {
-         res.status(500).send({
-            Error: error,
-         });
+      .catch(err => {
+         res.status(500).send({ error: 'Error at saving idea.' });
+         console.log(err);
       });
 };
 
-const posts = { postOne };
+const like = async (req, res) => {
+   const ideaId = mongoose.Types.ObjectId(req.params.ideaId);
+   const userId = mongoose.Types.ObjectId(req.userData._id);
+   const likeStatus = req.body.like;
+
+   if (likeStatus === true)
+      Idea.findByIdAndUpdate(
+         ideaId,
+         { $push: { likes: userId } },
+         { upsert: true }
+      )
+         .lean()
+         .exec()
+         .then(() => res.status(201).send({ likeStatus }))
+         .catch(error => {
+            console.log(error);
+            res.sendStatus(500);
+         });
+   else
+      Idea.findByIdAndUpdate(
+         ideaId,
+         { $pull: { likes: userId } },
+         { upsert: true }
+      )
+         .lean()
+         .exec()
+         .then(() => res.status(201).send({ likeStatus }))
+         .catch(error => {
+            console.log(error);
+            res.sendStatus(500);
+         });
+};
+
+const posts = { postIdea, like };
 
 module.exports = { ...gets, ...posts };
--- routes/idea.routes.js
+++ routes/idea.routes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
+const { tokenCheck } = require('../auth/googleOAuth.middleware');
 const controllers = require('./../controllers/idea.controllers');
 
 // GET /ideas
-router.get('/', controllers.getAll);
+router.get('/', controllers.getPaginated);
+router.get('/count', controllers.getCount);
+router.get('/:ideaId', controllers.getIdeaDetails);
+router.get('/:ideaId/like', tokenCheck, controllers.isLiked);
+router.get('/user/:userId', controllers.userIdeas);
 
 // POST /ideas
-router.post('/', controllers.postOne);
+router.post('/', tokenCheck, controllers.postIdea);
+router.post('/:ideaId/like', tokenCheck, controllers.like);
 
 module.exports = router;
